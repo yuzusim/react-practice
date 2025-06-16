d89e45fcb7551141ff30212ecc4ff6613f953692
yuzusim <yuzusim@gmail.com>
2025-06-17 02:00:20 +0900

    useEffect의 실행 조건 제어 방식 (의존성 배열 사용) 을 반복 학습

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,16 +8,23 @@ function App() {
   const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value); // 입력된 값을 상태로 업데이트
-  console.log("i run all the time");
+  // console.log("i run all the time");
+
   useEffect(() => {
-    console.log("CALL THE API");
+    console.log("i run only once"); // 나는 한번만 실행 되요
   }, []); // 빈 array를 써주었을 때 코드가 단 한번만 실행되는 이유
 
   useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("SEACH FOR", keyword);
-    }
+    console.log("i run 'keyword' changes."); // 나는 키워드가 변화할 때 실행되요
   }, [keyword]); // keyword가 변화할 때 마다 코드가 항상실행
+
+  useEffect(() => {
+    console.log("i run 'counter' changes."); // 나는 카운터가 변화할 때 실행되요
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("i run keyword & counter changes."); // 나는 키워드, 카운터 중 하나라도 변화할 때 실행되요
+  }, [keyword, counter]);
 
   return (
     <div>
