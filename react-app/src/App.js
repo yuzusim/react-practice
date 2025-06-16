600661ed893139d1236c6885fa3185c1ee08a86d
yuzusim <yuzusim@gmail.com>
2025-06-16 23:56:10 +0900

    API를 한 번만 호출하기 위한 목적” + “간단하게 함수 표현하려고 람다식을 쓴 것

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,10 +7,9 @@ function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
   console.log("i run all the time");
-  const iRunOnlyOnce = () => {
-    console.log("i run only thr once");
-  };
-  useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    console.log("CALL THE API");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
