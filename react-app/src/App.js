75357936f977395d38d831b429d2cab0a5721c72
yuzusim <yuzusim@gmail.com>
2025-06-17 00:52:58 +0900

    React에서 input을 제어하려면 value={state}와 정확한 onChange 핸들러를 함께 사용해야 제어 컴포넌트가 제대로 동작

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,13 +5,21 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value); // 입력된 값을 상태로 업데이트
   console.log("i run all the time");
   useEffect(() => {
     console.log("CALL THE API");
   }, []);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
