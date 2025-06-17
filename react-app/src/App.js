2c9056a014cb1301eda44322ad98a7c626f7cfbf
yuzusim <yuzusim@gmail.com>
2025-06-17 16:53:39 +0900

    useEffect 하나로 컴포넌트 생명주기(생성과 제거) 관리하는 법(리엑트 프젝에서는 useEffect 안에 모든걸 작성)

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,14 +4,27 @@ import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function Hello() {
-  function byFn() {
-    console.log("bye :(");
-  }
-  function hiFn() {
-    console.log("created :)"); // 컴포넌트 생성 시 실행
-    return byFn; // 컴포넌트 제거 시 실행할 함수 반환
-  }
-  useEffect(hiFn, []);
+  // function byFn() {
+  //   console.log("bye :(");
+  // }
+  // function hiFn() {
+  //   console.log("created :)"); // 컴포넌트 생성 시 실행
+  //   return byFn; // 컴포넌트 제거 시 실행할 함수 반환
+  // }
+
+  // 화살표 함수(람다식) 사용
+  useEffect(() => {
+    console.log("hi");
+    return () => console.log("bye :(");
+  }, []);
+
+  // 일반 함수(function 키워드) 사용
+  useEffect(function () {
+    console.log("hi :)");
+    return function () {
+      console.log("bye :(");
+    };
+  }, []);
   return <h1>Hello</h1>;
 }
 
