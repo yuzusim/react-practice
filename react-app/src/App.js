35159fe28af0cc28f4ef3ec8ab13a03da1907dda
yuzusim <yuzusim@gmail.com>
2025-06-17 15:20:50 +0900

    useState로 Hello 컴포넌트 토글 및 마운트 시 useEffect 한 번 실행

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,39 +3,20 @@ import styles from "./App.module.css";
 
 import { useState, useEffect } from "react";
 
+function Hello() {
+  useEffect(() => {
+    console.log("Im here!");
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value); // 입력된 값을 상태로 업데이트
-  // console.log("i run all the time");
-
-  useEffect(() => {
-    console.log("i run only once"); // 나는 한번만 실행 되요
-  }, []); // 빈 array를 써주었을 때 코드가 단 한번만 실행되는 이유
-
-  useEffect(() => {
-    console.log("i run 'keyword' changes."); // 나는 키워드가 변화할 때 실행되요
-  }, [keyword]); // keyword가 변화할 때 마다 코드가 항상실행
-
-  useEffect(() => {
-    console.log("i run 'counter' changes."); // 나는 카운터가 변화할 때 실행되요
-  }, [counter]);
-
-  useEffect(() => {
-    console.log("i run keyword & counter changes."); // 나는 키워드, 카운터 중 하나라도 변화할 때 실행되요
-  }, [keyword, counter]);
-
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
