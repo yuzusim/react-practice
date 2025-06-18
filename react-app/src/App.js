8cc2910a1a76f6ff5ccc3e972fc46498e98195e6
yuzusim <yuzusim@gmail.com>
2025-06-18 13:38:46 +0900

    간단한 To Do List 만들기 : 초기 코드 작성

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,40 +1,26 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+// import Button from "./Button";
+// import styles from "./App.module.css";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
-function Hello() {
-  // function byFn() {
-  //   console.log("bye :(");
-  // }
-  // function hiFn() {
-  //   console.log("created :)"); // 컴포넌트 생성 시 실행
-  //   return byFn; // 컴포넌트 제거 시 실행할 함수 반환
-  // }
-
-  // 화살표 함수(람다식) 사용
-  useEffect(() => {
-    console.log("hi");
-    return () => console.log("bye :(");
-  }, []);
-
-  // 일반 함수(function 키워드) 사용
-  useEffect(function () {
-    console.log("hi :)");
-    return function () {
-      console.log("bye :(");
-    };
-  }, []);
-  return <h1>Hello</h1>;
-}
+// input 생성 사용자 입력을 받음
+// useState("")를 사용해 toDo 상태를 빈 문자열로 초기화
+// value={toDo}로 상태값을 input에 바인딩해서, 상태가 변할 때 input 내용도 반영
+// onChange 이벤트 생성, <input onChange={onChange} />를 통해 사용자가 입력할 때마다 이벤트가 발생
+// const onChange = (event) => setToDo(event.target.value);는 입력한 값을 toDo 상태로 업데이트
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const onChange = (event) => setToDo(event.target.value);
+  console.log(toDo);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <input
+        type="text"
+        onChange={onChange}
+        value={toDo}
+        placeholder="Write your to do..."
+      />
     </div>
   );
 }
