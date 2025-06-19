589df2102c5c4824ffa2629949750f064fa6277e
yuzusim <yuzusim@gmail.com>
2025-06-20 04:26:50 +0900

    [movie] 각 경로(URL)마다 보여줄 컴포넌트를 지정해서 페이지 이동이 가능하게 하는 라우터 설정

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
       </Routes>
     </Router>
   );
