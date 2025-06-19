ef356e4c59cf0e2d97e6357cb414f8d07d8f41ef
yuzusim <yuzusim@gmail.com>
2025-06-19 19:32:00 +0900

    [movie] 짧은 버전 async-await

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,11 +3,22 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true); // 현재 로딩 중인지 아닌지를 나타내는 상태 변수 (초기값: true)
   const [movies, setMovies] = useState([]); // // 영화 데이터를 저장할 상태
+  // const getMovies = async () => {
+  //   const response = await fetch(
+  //     `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+  //   );
+  //   const json = await response.json();
+  //   setMovies(json.data.movies);
+  //   setLoading(false);
+  // };
+
+  // 짧은 버전 async-await
   const getMovies = async () => {
-    const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-    );
-    const json = await response.json();
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+      )
+    ).json();
     setMovies(json.data.movies);
     setLoading(false);
   };
