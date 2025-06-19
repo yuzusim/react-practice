8c885a691d97bda84b3a16087ea6a8a0edd29a5f
yuzusim <yuzusim@gmail.com>
2025-06-19 23:12:06 +0900

    [movie] summary, genres(장르) 화면 렌더링

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -34,7 +34,15 @@ function App() {
       ) : (
         <div>
           {movies.map((movie) => (
-            <div key={movie.id}>{movie.title}</div>
+            <div key={movie.id}>
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
           ))}
         </div>
       )}
