cdf1754e27163500f550e6b810def4cbafc189d3
yuzusim <yuzusim@gmail.com>
2025-06-19 18:38:19 +0900

    [movie] 로딩 상태를 true로 설정한 후, useEffect 안에서 컴포넌트가 처음 렌더링될 때 fetch로 API 데이터를 요청하고 .json()으로 파싱한 뒤 콘솔에 출력하는 코드

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,32 +1,18 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [loading, setLoading] = useState("true");
-  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true); // 현재 로딩 중인지 아닌지를 나타내는 상태 변수 (초기값: true)
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    // 컴포넌트가 처음 화면에 나타날 때 실행됨, [] : 의존성 배열이 비어 있으므로 처음 한 번만 실행됨
+    // 해당 URL로 HTTP 요청
+    fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+    )
+      .then((response) => response.json()) // 응답(response)을 JSON 형식으로 변환
+      .then((json) => console.log(json)); // 파싱된 JSON 데이터를 콘솔에 출력
   }, []);
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coins) => (
-            <option>
-              {coins.name} ({coins.symbol}): ${coins.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
-  );
+
+  return <div>{loading ? <h1>loading..</h1> : null}</div>;
 }
 
 export default App;
