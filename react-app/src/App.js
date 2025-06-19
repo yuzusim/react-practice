ec588c122bef48ecb8fa489bd48a126d5bc71503
yuzusim <yuzusim@gmail.com>
2025-06-19 14:16:53 +0900

    fetch와 useEffect를 사용해 컴포넌트가 처음 렌더링될 때 외부 API에서 데이터를 받아오는 법

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,43 +1,16 @@
-import { useState } from "react";
-
-// input 생성 사용자 입력을 받음
-// useState("")를 사용해 toDo 상태를 빈 문자열로 초기화
-// value={toDo}로 상태값을 input에 바인딩해서, 상태가 변할 때 input 내용도 반영
-// onChange 이벤트 생성, <input onChange={onChange} />를 통해 사용자가 입력할 때마다 이벤트가 발생
-// const onChange = (event) => setToDo(event.target.value);는 입력한 값을 toDo 상태로 업데이트
+import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState(""); // 입력값을 위한 상태
-  const [toDos, setToDos] = useState([]); // 여러 개의 할 일 목록을 저장하는 상태
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault(); // 폼 제출을 막기
-    if (toDo === "") {
-      return; // 아무것도 안했으면 중단
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]); //
-    setToDo(""); // 할 일을 입력한 후 input을 다시 비움 (초기화)
-  };
-  console.log(toDos); // toDos arry 확인
-
+  const [loading, setloading] = useState("trun");
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }, []);
   return (
     <div>
-      <h1>My To Dos({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          onChange={onChange}
-          value={toDo}
-          placeholder="Write your to do..."
-        />
-        <button>Add TO DO</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins!</h1>
+      {loading ? <strong>Loading...</strong> : null}
     </div>
   );
 }
