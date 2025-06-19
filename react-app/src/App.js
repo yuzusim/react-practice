383623d81a65dea09a5ae714bd37fb71a2f1bd30
yuzusim <yuzusim@gmail.com>
2025-06-19 17:59:04 +0900

    최종수정

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [loading, setLoading] = useState("trun");
+  const [loading, setLoading] = useState("true");
   const [coins, setCoins] = useState([]);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
