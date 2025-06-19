227bb06de830f9c4e0171007a4a2a3e6a398b84b
yuzusim <yuzusim@gmail.com>
2025-06-20 01:23:19 +0900

    [movie] routes와 components 폴더를 분리하고, React Router를 활용해 페이지 단위 라우팅 구조를 설계

---
+++ react-app/src/routes/Home.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import Movie from "../components/Movie";
+
+function Home() {
+  const [loading, setLoading] = useState(true); // 현재 로딩 중인지 아닌지를 나타내는 상태 변수 (초기값: true)
+  const [movies, setMovies] = useState([]); // // 영화 데이터를 저장할 상태
+  // const getMovies = async () => {
+  //   const response = await fetch(
+  //     `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+  //   );
+  //   const json = await response.json();
+  //   setMovies(json.data.movies);
+  //   setLoading(false);
+  // };
+
+  // 짧은 버전 async-await
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        <h1>loading..</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
