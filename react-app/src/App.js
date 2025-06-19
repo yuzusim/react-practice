8d74d87c28793ef10369ddfd3376612bbed6157d
yuzusim <yuzusim@gmail.com>
2025-06-20 03:41:42 +0900

    [movie] react-router-dom 패키지 설치, package.json 있는 폴더에서 설치해야 node_modules가 생성되고 모듈이 제대로 import 할 수 있다.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,15 @@
-import { useState, useEffect } from "react";
-import Movie from "./components/Movie";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./routes/Home";
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
