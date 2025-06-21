fdcb8a740c64111f27cd6f23f024148920606285
yuzusim <yuzusim@gmail.com>
2025-06-22 00:46:29 +0900

    [movie] useEffect 안에서 비동기 작업을 하려면, async 함수(getMovie)를 만들어서 따로 호출

--- react-app/src/routes/Detail.js
+++ react-app/src/routes/Detail.js
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
   const { id } = useParams(); // URL에서 id 값 꺼냄
-  console.log(id);
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
   return <h1>Detail</h1>;
 }
 
