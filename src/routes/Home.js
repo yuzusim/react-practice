a9d11e3411ffac060f55646f977a6c968acf4c6c
yuzusim <yuzusim@gmail.com>
2025-06-22 21:52:33 +0900

    [CSS] Home.module.css 생성후 Home.js 에 import, className={} 주기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
   const [loading, setLoading] = useState(true); // 현재 로딩 중인지 아닌지를 나타내는 상태 변수 (초기값: true)
@@ -29,11 +30,13 @@ function Home() {
 
   console.log(movies);
   return (
-    <div>
+    <div className={styles.container}>
       {loading ? (
-        <h1>loading..</h1>
+        <div className={styles.loader}>
+          <h1>loading..</h1>
+        </div>
       ) : (
-        <div>
+        <div className={styles.movies}>
           {movies.map((movie) => (
             <Movie
               key={movie.id}
