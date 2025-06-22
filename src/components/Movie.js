46cbc382763f43f718a991de1b1cce2d5cf67b0b
yuzusim <yuzusim@gmail.com>
2025-06-22 22:14:01 +0900

    summary.length > 170 수정

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -14,7 +14,7 @@ function Movie({ id, coverImg, title, year, summary, genres }) {
           {/* <Link to="/movie">{title}</Link> */}
         </h2>
         <h3 className={styles.movie__year}>{year}</h3>
-        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+        <p>{summary.length > 170 ? `${summary.slice(0, 170)}...` : summary}</p>
         <ul className={styles.movie__genres}>
           {genres.map((g) => (
             <li key={g}>{g}</li>
